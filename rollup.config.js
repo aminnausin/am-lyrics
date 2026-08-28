4b5ea383a91006f3d4d996d5e15e64b4da5a0a6c
aminnausin <aminnausin@outlook.com>
2026-08-28 12:45:07 -0400

    chore: remove react from build

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
-import resolve from '@rollup/plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript'
-import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import babel from '@rollup/plugin-babel';
 
-const PACKAGE_ROOT_PATH = process.cwd()
+const PACKAGE_ROOT_PATH = process.cwd();
 
 const commonPlugins = [
   resolve(),
   typescript({
-    tsconfig: `./tsconfig.json`
+    tsconfig: `./tsconfig.json`,
   }),
   babel({
     exclude: 'node_modules/**',
     rootMode: 'upward',
-    babelHelpers: 'runtime'
-  })
+    babelHelpers: 'runtime',
+  }),
 ];
 
 export default [
@@ -26,21 +26,21 @@ export default [
         file: 'dist/src/am-lyrics.js',
         format: 'esm',
         sourcemap: true,
-      }
+      },
     ],
-    plugins: commonPlugins
+    plugins: commonPlugins,
   },
   // Build for React
-  {
-    input: `${PACKAGE_ROOT_PATH}/src/react.ts`,
-    external: [/@babel\/runtime/, 'react', '@lit/react'],
-    output: [
-      {
-        file: 'dist/src/react.js',
-        format: 'esm',
-        sourcemap: true,
-      }
-    ],
-    plugins: commonPlugins
-  }
-]
+  // {
+  //   input: `${PACKAGE_ROOT_PATH}/src/react.ts`,
+  //   external: [/@babel\/runtime/, 'react', '@lit/react'],
+  //   output: [
+  //     {
+  //       file: 'dist/src/react.js',
+  //       format: 'esm',
+  //       sourcemap: true,
+  //     }
+  //   ],
+  //   plugins: commonPlugins
+  // }
+];
